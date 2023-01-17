ac3b362e110a229136b7b715e1984ee28b807f06
Mariu <mparreno1@educa.madrid.org>
2023-01-17 20:40:19 +0100

    version 1

---
+++ HolaUniverso.java
@@ -0,0 +1,11 @@
+package edu.piobaroja.dam1.programacion.holauniverso;
+
+public class HolaUniverso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola Universo");
+		
+	}
+
+}
